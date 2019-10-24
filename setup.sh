5552f9c821355857ef33bca0bbfedbee967663da
nobu-g <ueda-n127@hotmail.co.jp>
2019-10-24 17:51:32 +0900

    update

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+mkdir -p ~/.emacs.d
+mkdir -p ~/.config
+mkdir -p ~/.config/git
+mkdir -p ~/.config/peco
+
 ln -sf ~/dotfiles/.zsh.d/.zshenv ~/.zshenv
 ln -sf ~/dotfiles/.zsh.d/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/.zsh.d/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.emacs.d/init.el ~/.emacs.d/init.el
 ln -sf ~/dotfiles/.git.d/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.git.d/.gitignore_global ~/.config/git/ignore
+ln -sf ~/dotfiles/.peco/config.json ~/.config/peco/config.json
