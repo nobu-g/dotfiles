92facd496c5d67e8156eb61fad2b6ef38d2178d9
nobu-g <ueda-n127@hotmail.co.jp>
2021-03-05 00:18:58 +0900

    remove doom themes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -227,32 +227,3 @@
 
 (provide '.emacs)
 ;;; .emacs ends here
-
-
-;; modeline
-;; https://qiita.com/Ladicle/items/feb5f9dce9adf89652cf
-(use-package doom-themes
-  :custom
-  (doom-themes-enable-italic t)
-  (doom-themes-enable-bold t)
-  :custom-face
-  (doom-modeline-bar ((t (:background "#6272a4"))))
-  :config
-  (load-theme 'doom-vibrant t)
-  (doom-themes-neotree-config)
-  (doom-themes-org-config))
-
-(use-package doom-modeline
-  :custom
-  (doom-modeline-buffer-file-name-style 'truncate-with-project)
-  (doom-modeline-icon nil)
-  (doom-modeline-major-mode-icon nil)
-  (doom-modeline-minor-modes nil)
-  :hook
-  (after-init . doom-modeline-mode)
-  :config
-  (line-number-mode 0)
-  (column-number-mode 0)
-  (doom-modeline-def-modeline 'main
-    '(bar window-number matches buffer-info remote-host buffer-position parrot selection-info)
-    '(misc-info persp-name lsp github debug minor-modes input-method major-mode process vcs checker)))
