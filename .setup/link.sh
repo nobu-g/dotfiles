9db613afb7a5ce2bc2ad817738b2f381ee31a5e6
nobu-g <ueda-n127@hotmail.co.jp>
2021-03-21 19:43:57 +0900

    refactor

--- .setup/link.sh
+++ .setup/link.sh
@@ -1,31 +1,38 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -exu
 
-ln -sf ~/dotfiles/.zsh.d/.zshenv ~/.zshenv
-ln -sf ~/dotfiles/.zsh.d/.zprofile ~/.zprofile
-ln -sf ~/dotfiles/.zsh.d/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.zsh.d/.p10k.zsh ~/.p10k.zsh
-ln -sf ~/dotfiles/.git.d/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/.config/git ~/.config/
-ln -sf ~/dotfiles/.config/peco ~/.config/
-ln -sf ~/dotfiles/.config/fsh ~/.config/
-ln -sf ~/dotfiles/.config/bat ~/.config/
-ln -sf ~/dotfiles/.latexmkrc ~/.latexmkrc
-ln -sf ~/dotfiles/bin/line ~/scripts/
+DOTDIR=$(
+  cd "$(dirname "$(dirname "$0")")"
+  pwd
+)
+cd "$DOTDIR"
+
+ZDOTDIR=${DOTDIR%/}/.zsh.d
+for f in "${ZDOTDIR%/}"/{.zshenv,.zprofile,.zshrc,.p10k.zsh}; do
+  ln -snfv "$f" "$HOME"
+done
+
+for f in "${DOTDIR%/}"/.config/*; do
+  ln -snfv "$f" "$HOME/.config"
+done
+
+ln -sf "${DOTDIR%/}"/.git.d/.gitconfig "$HOME/.gitconfig"
+ln -sf "${DOTDIR%/}"/.latexmkrc "$HOME/.latexmkrc"
+ln -sf "${DOTDIR%/}"/bin/line "$HOME/scripts/"
 
 case "${OSTYPE}" in
 linux* | cygwin*)
-  ln -sf ~/dotfiles/.emacs.d/init.el ~/.emacs.d/init.el
+  ln -sf "${DOTDIR%/}"/.emacs.d/init.el "$HOME/.emacs.d/init.el"
   ;;
 freebsd* | darwin*)
   ln -sf /usr/local/share/git-core/contrib/diff-highlight/diff-highlight /usr/local/bin
-  ln -sf ~/dotfiles/.doom.d ~/.doom.d
+  ln -sf "${DOTDIR%/}"/.doom.d "$HOME/.doom.d"
   ;;
 esac
 
 if [[ -e ${HOME}/.linuxbrew/share/git-core/contrib/diff-highlight/diff-highlight ]]; then
-  ln -sf "${HOME}"/.linuxbrew/share/git-core/contrib/diff-highlight/diff-highlight "${HOME}"/.linuxbrew/bin
+  ln -sf "$HOME"/.linuxbrew/share/git-core/contrib/diff-highlight/diff-highlight "$HOME/.linuxbrew/bin"
 fi
 if [[ -e /home/.linuxbrew/share/git-core/contrib/diff-highlight/diff-highlight ]]; then
   ln -sf /home/.linuxbrew/share/git-core/contrib/diff-highlight/diff-highlight /home/.linuxbrew/bin
