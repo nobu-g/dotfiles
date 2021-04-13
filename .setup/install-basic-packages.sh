562b054c305b1c205c1e7c135426b3cae73cfdac
nobu-g <ueda-n127@hotmail.co.jp>
2021-04-13 19:07:08 +0900

    add libc-dev for alpine

--- .setup/install-basic-packages.sh
+++ .setup/install-basic-packages.sh
@@ -18,7 +18,7 @@ if [[ $distro == "redhat" ]]; then
 elif [[ $distro == "arch" ]]; then
   sudo pacman -S --noconfirm --needed tar
 elif [[ $distro == "alpine" ]]; then
-  sudo apk add g++ procps coreutils grep
+  sudo apk add libc-dev g++ procps coreutils grep
 elif [[ $distro == "debian" ]]; then
   :
 else
