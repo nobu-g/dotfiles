b139058f217dd6d0914883b91dac3b966c24f378
nobu-g <ueda-n127@hotmail.co.jp>
2021-05-02 21:31:36 +0900

    fix

--- .setup/python-packages.sh
+++ .setup/python-packages.sh
@@ -6,7 +6,7 @@ pip3 install --user autopep8
 pip3 install --user trash-cli
 pip3 install --user speedtest-cli
 # https://github.com/googlefonts/pyfontaine/issues/109#issuecomment-604872347
-export PATH="$BREW_PREFIX/opt/icu4c/{bin,sbin}:$PATH"
+export PATH="$BREW_PREFIX/opt/icu4c/bin:$BREW_PREFIX/opt/icu4c/sbin:$PATH"
 export PKG_CONFIG_PATH="$BREW_PREFIX/opt/icu4c/lib/pkgconfig"
 ls "$BREW_PREFIX/opt/icu4c/bin" # debug print
 pip3 install --user csvkit
