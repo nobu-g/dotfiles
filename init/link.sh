c11784a4c241481aec8d2cd1fa8e43bff943953b
nobu-g <ueda-n127@hotmail.co.jp>
2021-05-15 02:28:25 +0900

    fix a bug

--- init/link.sh
+++ init/link.sh
@@ -9,23 +9,23 @@ BASE_DIR=$(
 cd "$BASE_DIR"
 
 for f in "${BASE_DIR%/}"/.zsh.d/{.zshenv,.zprofile,.zshrc,.p10k.zsh}; do
-  ln -snfv "$f" "$HOME/"
+  ln -snfv "$f" "$HOME"
 done
 
 for f in "${BASE_DIR%/}"/.config/*; do
-  ln -snfv "$f" "$HOME/.config/"
+  ln -snfv "$f" "$HOME/.config"
 done
 
-ln -snfv "${BASE_DIR%/}/.latexmkrc" "$HOME/"
-ln -snfv "${BASE_DIR%/}/bin/line" "$HOME/scripts/"
-ln -snfv "${BASE_DIR%/}/bin/line-msg" "$HOME/scripts/"
-ln -snfv "${BASE_DIR%/}/bin/pyshow" "$HOME/scripts/"
+ln -snfv "${BASE_DIR%/}/.latexmkrc" "$HOME"
+ln -snfv "${BASE_DIR%/}/bin/line" "$HOME/scripts"
+ln -snfv "${BASE_DIR%/}/bin/line-msg" "$HOME/scripts"
+ln -snfv "${BASE_DIR%/}/bin/pyshow" "$HOME/scripts"
 
 case "${OSTYPE}" in
 linux* | cygwin*)
-  ln -snfv "${BASE_DIR%/}/.emacs.d/init.el" "$HOME/.emacs.d/"
+  ln -snfv "${BASE_DIR%/}/.emacs.d/init.el" "$HOME/.emacs.d"
   ;;
 freebsd* | darwin*)
-  ln -snfv "${BASE_DIR%/}/.doom.d" "$HOME/"
+  ln -snfv "${BASE_DIR%/}/.doom.d" "$HOME"
   ;;
 esac
