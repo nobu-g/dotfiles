a6c3d9917bb0afc21ec32f502e6ff7af2d110e14
nobu-g <ueda-n127@hotmail.co.jp>
2021-05-17 02:23:18 +0900

    do not add path under $HOME to /etc/shells

--- init/setup-shell.sh
+++ init/setup-shell.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 
-[[ -n "$(command -v brew)" ]] && zsh_path="$(brew --prefix)/bin/zsh" || zsh_path="$(command -v zsh)"
+set -u
+
+# set zsh_path only when zsh installed via Homebrew is not under $HOME
+brew_prefix=$(realpath "${HOMEBREW_PREFIX}")
+if (type brew &>/dev/null) && [[ -n ${brew_prefix##$(realpath "${HOME}")/*} ]]; then
+  zsh_path="${HOMEBREW_PREFIX}/bin/zsh"
+else
+  zsh_path="$(command -v zsh)"
+  for p in $(which -a zsh); do
+    if [[ -n ${p##$(realpath "${HOME}")/*} ]]; then
+      zsh_path="${p}"
+      break
+    fi
+  done
+fi
+
 if ! grep "$zsh_path" /etc/shells; then
   echo "adding $zsh_path to /etc/shells"
   echo "$zsh_path" | sudo tee -a /etc/shells
