41ca122ce15ecc15a2a0284cca31fe8bee54236c
nobu-g <ueda-n127@hotmail.co.jp>
2021-06-13 02:01:31 +0900

    install some rust crates via homebrew

--- init/rust-packages.sh
+++ init/rust-packages.sh
@@ -4,16 +4,14 @@ set -u
 
 # https://wonderwall.hatenablog.com/entry/rust-command-line-tools
 
-# crates installed with zinit
-# ripgrep, fd, bat, exa, delta
+# crates installed via zinit:
+#   ripgrep, fd, bat, delta
+# crates installed via homebrew:
+#   xsv, lsd, fd, ripgrep, procs, sd, hyperfine, pastel, du-dust
 
-cargo install hyperfine # comman-line benchmark tool
-cargo install xsv
 cargo install gping         # Ping, but with a graph
 cargo install hexyl         # A command-line hex viewer
 cargo install broot         # A new way to see and navigate directory trees
-cargo install pastel        # color manipulation
 cargo install watchexec-cli # monitor file change
-cargo install du-dust       # A more intuitive version of du in rust
 cargo install pueue         # process queue
 # cargo install mcfly     # better history search
