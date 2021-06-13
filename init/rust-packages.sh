5c7404d3627a6065757739cdd6d6ebe7164c80c9
nobu-g <ueda-n127@hotmail.co.jp>
2021-06-13 15:04:20 +0900

    install hexyl and broot via homebrew

--- init/rust-packages.sh
+++ init/rust-packages.sh
@@ -7,11 +7,9 @@ set -u
 # crates installed via zinit:
 #   ripgrep, fd, bat, delta
 # crates installed via homebrew:
-#   xsv, lsd, fd, ripgrep, ripgrep-all, procs, sd, hyperfine, pastel, du-dust
+#   xsv, lsd, fd, ripgrep, ripgrep-all, procs, sd, hyperfine, pastel, du-dust, hexyl, broot
 
 cargo install gping         # Ping, but with a graph
-cargo install hexyl         # A command-line hex viewer
-cargo install broot         # A new way to see and navigate directory trees
 cargo install watchexec-cli # monitor file change
 cargo install pueue         # process queue
 # cargo install mcfly     # better history search
