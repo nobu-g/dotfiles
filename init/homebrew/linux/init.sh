5829f47abf43d9163c37d4dae949ed38713c888b
nobu-g <ueda-n127@hotmail.co.jp>
2021-06-14 22:04:58 +0900

    remove needless commands

--- init/homebrew/linux/init.sh
+++ init/homebrew/linux/init.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/env bash
 
-# Install linuxbrew
+set -eu
+
+# Install Linuxbrew
 git clone --depth 1 https://github.com/Homebrew/brew "${HOMEBREW_PREFIX}/Homebrew"
 mkdir -p "${HOMEBREW_PREFIX}/bin"
 ln -fs "${HOMEBREW_PREFIX}/Homebrew/bin/brew" "${HOMEBREW_PREFIX}/bin"
-eval "$("${HOMEBREW_PREFIX}/bin/brew" shellenv)"
-
-# Make sure we’re using the latest Linuxbrew.
-brew update
-
-# Upgrade any already-installed formulae.
-brew upgrade
-
-# Remove outdated versions from the cellar.
-brew cleanup
+# eval "$("${HOMEBREW_PREFIX}/bin/brew" shellenv)"
--- init/homebrew/macos/init.sh
+++ init/homebrew/macos/init.sh
@@ -1,20 +1,13 @@
 #!/usr/bin/env bash
 
-# Install homebrew
+set -eu
+
+# Install Homebrew
 if [[ ${HOMEBREW_PREFIX} == "/usr/local" ]]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
   git clone --depth 1 https://github.com/Homebrew/brew "${HOMEBREW_PREFIX}/Homebrew"
   mkdir -p "${HOMEBREW_PREFIX}/bin"
   ln -fs "${HOMEBREW_PREFIX}/Homebrew/bin/brew" "${HOMEBREW_PREFIX}/bin"
-  eval "$("${HOMEBREW_PREFIX}/bin/brew" shellenv)"
+  # eval "$("${HOMEBREW_PREFIX}/bin/brew" shellenv)"
 fi
-
-# Make sure we’re using the latest Homebrew.
-brew update
-
-# Upgrade any already-installed formulae.
-brew upgrade
-
-# Remove outdated versions from the cellar.
-brew cleanup
