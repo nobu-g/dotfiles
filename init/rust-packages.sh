f82d79701e2a0c5d2e5a02c30b4cbc280c67a1fb
nobu-g <ueda-n127@hotmail.co.jp>
2021-11-30 09:30:17 +0900

    tweak

--- init/rust-packages.sh
+++ init/rust-packages.sh
@@ -2,10 +2,5 @@
 
 set -u
 
-# https://wonderwall.hatenablog.com/entry/rust-command-line-tools
-
-# crates installed via zinit:
-#   ripgrep, fd, bat, delta
-
 cargo install cargo-cache   # cargo cache cleaner
 # cargo install mcfly     # better history search
