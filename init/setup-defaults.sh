230b4b67a781615e28d9e1e2cac6792992b6aee3
nobu-g <ueda-n127@hotmail.co.jp>
2021-12-13 15:21:35 +0900

    disable hotcorners

--- init/setup-defaults.sh
+++ init/setup-defaults.sh
@@ -114,11 +114,11 @@ defaults write com.apple.screencapture type -string "png"
 #defaults write com.apple.dock wvous-tl-corner -int 2
 #defaults write com.apple.dock wvous-tl-modifier -int 0
 # Top right screen corner → Desktop
-defaults write com.apple.dock wvous-tr-corner -int 4
-defaults write com.apple.dock wvous-tr-modifier -int 0
+# defaults write com.apple.dock wvous-tr-corner -int 4
+# defaults write com.apple.dock wvous-tr-modifier -int 0
 # Bottom left screen corner → Start screen saver
-defaults write com.apple.dock wvous-bl-corner -int 5
-defaults write com.apple.dock wvous-bl-modifier -int 0
+# defaults write com.apple.dock wvous-bl-corner -int 5
+# defaults write com.apple.dock wvous-bl-modifier -int 0
 
 ###############################################################################
 # Activity Monitor                                                            #
