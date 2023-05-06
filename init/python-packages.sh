61889ae755229b5f31345b812f726fa2d4d960da
nobu-g <ueda-n127@hotmail.co.jp>
2023-05-07 00:22:29 +0900

    specify python path for pipx install

--- init/python-packages.sh
+++ init/python-packages.sh
@@ -5,21 +5,25 @@ set -u
 # packages to import from everywhere
 pip3 install --user rhoknp
 
+_pipx_install() {
+  pipx install "$1" --python "${HOMEBREW_PREFIX}/opt/python@3.11/bin/python3.11"
+}
+
 # command-line tools
-pipx install virtualenv
-pipx install autopep8
-pipx install flake8
-pipx install trash-cli
-pipx install speedtest-cli
+_pipx_install virtualenv
+_pipx_install autopep8
+_pipx_install flake8
+_pipx_install trash-cli
+_pipx_install speedtest-cli
 # https://github.com/googlefonts/pyfontaine/issues/109#issuecomment-604872347
 export PATH="${HOMEBREW_PREFIX}/opt/icu4c/bin:${HOMEBREW_PREFIX}/opt/icu4c/sbin:$PATH"
 export PKG_CONFIG_PATH="${HOMEBREW_PREFIX}/opt/icu4c/lib/pkgconfig"
-pipx install csvkit
-pipx install pipenv
-pipx install poetry
-pipx install kyoto-reader
-pipx install ipython      # Interactive computing in Python
-pipx install py-spy       # Sampling profiler for Python programs
-pipx install httpie       # User-friendly cURL replacement (command-line HTTP client)
-pipx install ansible-core # Automate deployment, configuration, and upgrading
-pipx install pre-commit   # Framework for managing multi-language pre-commit hooks
+_pipx_install csvkit
+_pipx_install pipenv
+_pipx_install poetry
+_pipx_install kyoto-reader
+_pipx_install ipython      # Interactive computing in Python
+_pipx_install py-spy       # Sampling profiler for Python programs
+_pipx_install httpie       # User-friendly cURL replacement (command-line HTTP client)
+_pipx_install ansible-core # Automate deployment, configuration, and upgrading
+_pipx_install pre-commit   # Framework for managing multi-language pre-commit hooks
