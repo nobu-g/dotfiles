3c0442b919f62bd2eee2f2661deb4119726648f5
nobu-g <ueda-n127@hotmail.co.jp>
2023-05-11 20:03:51 +0900

    fix .pythonrc.py

--- .config/python/.pythonrc.py
+++ .config/python/.pythonrc.py
@@ -1,31 +1,27 @@
 # Make sure the following line exists in your `.zshrc`
 # export PYTHONSTARTUP="$HOME/.config/.pythonrc.py"
 
-import sys
-import os
-import re
-import json
+import sys, os, re, json  # noqa
 from pathlib import Path
-from typing import *
-from collections import defaultdict
+from collections import defaultdict # noqa
+from typing import * # noqa
 
-print('Imported sys, os, re, json, Path, defaultdict, and all modules of typing.')
+print('Imported sys, os, re, json, Path, defaultdict, and all types from typing.')
 
 try:
-    from rhoknp import *
-    print('Imported all modules of rhoknp.')
+    from rhoknp import * # noqa
+    print('Imported all modules etc. from rhoknp.')
 except ImportError:
     print("rhoknp not available")
 
 try:
-    from transformers import AutoConfig, AutoModel, AutoTokenizer
+    from transformers import AutoConfig, AutoModel, AutoTokenizer # noqa
     print('Imported AutoConfig, AutoModel, and AutoTokenizer from transformers.')
 except ImportError:
     pass
 
 # https://unix.stackexchange.com/questions/630642/change-location-of-python-history
-import readline
-import atexit
+import readline, atexit # noqa
 
 # If no history was loaded, default to .python_history.
 # The guard is necessary to avoid doubling history size at
@@ -38,12 +34,15 @@ try:
 except OSError:
     pass
 
+
 def write_history():
+    import readline
     try:
         readline.write_history_file(__hist)
     except OSError:
         # bpo-19891, bpo-41193: Home directory does not exist
         # or is not writable, or the filesystem is read-only.
         pass
+
 
 atexit.register(write_history)
