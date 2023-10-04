1826061961b2618aa1868718a504849400c54de0
nobu-g <ueda-n127@hotmail.co.jp>
2023-10-04 21:50:19 +0900

    add pipx packages

--- init/python-packages.sh
+++ init/python-packages.sh
@@ -10,7 +10,7 @@ _pipx_install() {
 }
 
 # command-line tools
-_pipx_install virtualenv
+_pipx_install virtualenv # Virtual Python Environment builder
 _pipx_install autopep8
 _pipx_install flake8
 _pipx_install trash-cli
@@ -19,11 +19,14 @@ _pipx_install speedtest-cli
 export PATH="${HOMEBREW_PREFIX}/opt/icu4c/bin:${HOMEBREW_PREFIX}/opt/icu4c/sbin:$PATH"
 export PKG_CONFIG_PATH="${HOMEBREW_PREFIX}/opt/icu4c/lib/pkgconfig"
 _pipx_install csvkit
-_pipx_install pipenv
-_pipx_install poetry
+_pipx_install pipenv # Python Development Workflow for Humans.
+_pipx_install poetry # Python dependency management and packaging made easy.
 _pipx_install kyoto-reader
-_pipx_install ipython      # Interactive computing in Python
-_pipx_install py-spy       # Sampling profiler for Python programs
-_pipx_install httpie       # User-friendly cURL replacement (command-line HTTP client)
-_pipx_install ansible-core # Automate deployment, configuration, and upgrading
-_pipx_install pre-commit   # Framework for managing multi-language pre-commit hooks
+_pipx_install ipython          # Interactive computing in Python
+_pipx_install py-spy           # Sampling profiler for Python programs
+_pipx_install httpie           # User-friendly cURL replacement (command-line HTTP client)
+_pipx_install ansible-core     # Automate deployment, configuration, and upgrading
+_pipx_install pre-commit       # Framework for managing multi-language pre-commit hooks
+_pipx_install kwja             # A unified Japanese analyzer based on foundation models
+_pipx_install openai-whisper   # Robust Speech Recognition via Large-Scale Weak Supervision
+_pipx_install open-interpreter # Let language models run code locally.
