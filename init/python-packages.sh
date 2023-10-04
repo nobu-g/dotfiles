f76b12e869b50a4e338790335a26a5ecc890c7ef
nobu-g <ueda-n127@hotmail.co.jp>
2023-10-04 21:52:53 +0900

    update python version for pipx packages

--- init/python-packages.sh
+++ init/python-packages.sh
@@ -5,27 +5,31 @@ set -u
 # packages to import from everywhere
 pip3 install --user rhoknp
 
-_pipx_install() {
+_pipx_install_py311() {
   pipx install "$1" --python "${HOMEBREW_PREFIX}/opt/python@3.11/bin/python3.11"
 }
 
+_pipx_install_py312() {
+  pipx install "$1" --python "${HOMEBREW_PREFIX}/opt/python@3.12/bin/python3.12"
+}
+
 # command-line tools
-_pipx_install virtualenv # Virtual Python Environment builder
-_pipx_install autopep8
-_pipx_install flake8
-_pipx_install trash-cli
-_pipx_install speedtest-cli
+_pipx_install_py312 virtualenv # Virtual Python Environment builder
+_pipx_install_py312 autopep8
+_pipx_install_py312 flake8
+_pipx_install_py312 trash-cli
+_pipx_install_py312 speedtest-cli
 # https://github.com/googlefonts/pyfontaine/issues/109#issuecomment-604872347
 export PATH="${HOMEBREW_PREFIX}/opt/icu4c/bin:${HOMEBREW_PREFIX}/opt/icu4c/sbin:$PATH"
 export PKG_CONFIG_PATH="${HOMEBREW_PREFIX}/opt/icu4c/lib/pkgconfig"
-_pipx_install csvkit
-_pipx_install pipenv           # Python Development Workflow for Humans.
-_pipx_install poetry           # Python dependency management and packaging made easy.
-_pipx_install ipython          # Interactive computing in Python
-_pipx_install py-spy           # Sampling profiler for Python programs
-_pipx_install httpie           # User-friendly cURL replacement (command-line HTTP client)
-_pipx_install ansible-core     # Automate deployment, configuration, and upgrading
-_pipx_install pre-commit       # Framework for managing multi-language pre-commit hooks
-_pipx_install kwja             # A unified Japanese analyzer based on foundation models
-_pipx_install openai-whisper   # Robust Speech Recognition via Large-Scale Weak Supervision
-_pipx_install open-interpreter # Let language models run code locally.
+_pipx_install_py312 csvkit
+_pipx_install_py312 pipenv           # Python Development Workflow for Humans.
+_pipx_install_py312 poetry           # Python dependency management and packaging made easy.
+_pipx_install_py312 ipython          # Interactive computing in Python
+_pipx_install_py312 py-spy           # Sampling profiler for Python programs
+_pipx_install_py312 httpie           # User-friendly cURL replacement (command-line HTTP client)
+_pipx_install_py312 ansible-core     # Automate deployment, configuration, and upgrading
+_pipx_install_py312 pre-commit       # Framework for managing multi-language pre-commit hooks
+_pipx_install_py311 kwja             # A unified Japanese analyzer based on foundation models
+_pipx_install_py311 openai-whisper   # Robust Speech Recognition via Large-Scale Weak Supervision
+_pipx_install_py311 open-interpreter # Let language models run code locally.
